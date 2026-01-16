20a3ef9c844a860461a8a3b900ee9e6dceedc6da
hanseldsouza02 <hanseldsouza02@gmail.com>
2026-01-16 10:12:02 +0530

    rotation of array question

--- Array/src/RotateArray.java
+++ Array/src/RotateArray.java
@@ -13,9 +13,20 @@ class Solution {
     }
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k =k%n;
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
+        k = k % n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+
+
+        //BRUTE FORCE
+
+        for (int i = 0; i < k; i++) {
+             int last = nums[n-1];
+            for(int j = n-1;j>0;i++){
+                nums[j] = nums[j-1];
+            }
+            nums[0] = last;
+        }
     }
 }
