c0587145f56f071aaeb45fd38cbe5d3890ddebac
hanseldsouza02 <hanseldsouza02@gmail.com>
2026-01-16 15:16:02 +0530

    Checking array is sorted or not

---
+++ Array/src/DuplicateSortedArray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //BRUTE FORCE SOLUTION
+//        int n = nums.length;
+//        int count = 1;
+//        for(int i =1;i<n;i++){
+//           if(nums[i] != nums[i-1]){
+//                nums[count] = nums[i];
+//                count++;
+//        }
+//        return count;
+//   }
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int count = 1;
+        while(i<n){
+            if(nums[i] == nums[j]) {
+                i++;
+            }else if(nums[i] != nums[j]){
+                nums[count] = nums[i];
+                count++;
+                j = i;
+            }
+        }
+        return count;
+
+    }
+}
