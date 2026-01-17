dab6b707f9f9eb44e349a4f4883f20564513a6b7
hanseldsouza02 <hanseldsouza02@gmail.com>
2026-01-17 15:44:48 +0530

    rotating an element to the left

--- Array/src/RotateArray.java
+++ Array/src/RotateArray.java
@@ -20,6 +20,18 @@ class Solution {
 
 
         //BRUTE FORCE
+        int n = nums.length;
+        k = k%n;
+        int[] temp = new int[k];
+        for(int i =0;i<k;i++){
+            temp[i] = nums[n-k+i];
+        }
+        for(int i = n-k-1 ;i>=0;i-- ){
+            nums[i+k] = nums[i];
+        }
+        for(int i = 0;i<k;i++ ){
+            nums[i] = temp[i];
+        }
 
         for (int i = 0; i < k; i++) {
              int last = nums[n-1];
