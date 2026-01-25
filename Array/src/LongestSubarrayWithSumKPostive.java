3a9ad6bf967dedd5eeb09a790571035555487cd5
hanseldsouza02 <hanseldsouza02@gmail.com>
2026-01-25 21:52:24 +0530

    logest subarray with sum equal to k with only positive numbers

--- Array/src/LongestSubarrayWithSumKPostive.java
+++ Array/src/LongestSubarrayWithSumKPostive.java
@@ -1,5 +1,8 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+
+        //BRUTE FORCE SOLUTION
+
         int n = nums.length;
         int len = 0;
         for(int i =0;i<n;i++){
@@ -13,8 +16,24 @@ class Solution {
                 }
             }
         }
-
         return len;
+
+        //BETTER SOLUTION O(N^2)
+
+        int n = nums.length;
+        int len = 0;
+        for(int i =0;i<n;i++){
+            int s =0;
+            for(int j = i;j<n;j++){
+                s += nums[j];
+                if(s == k){
+                        len = Math.max(len,j-i+1);
+                    }
+                }
+            }
+        }
+        return len;
+
 
     }
 }
