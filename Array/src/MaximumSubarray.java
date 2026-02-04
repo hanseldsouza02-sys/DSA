d3d819f8950e1ff483b4abad7549f84109a38d0b
hanseldsouza02 <hanseldsouza02@gmail.com>
2026-02-04 07:40:58 +0530

    findind the maximum subbarray

---
+++ Array/src/MaximumSubarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int cursum = nums[0];
+        int maxsum = nums[0];
+
+        for(int i =1;i<n;i++){
+            cursum = Math.max(nums[i],cursum+nums[i]);
+            maxsum = Math.max(maxsum,cursum);
+
+        }
+        return maxsum;
+    }
+}
